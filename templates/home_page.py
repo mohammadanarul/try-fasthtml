5a2c4667e7dca6c082041c1e66ef195a1a1acb37
anaruldev <anarul.tech@aamartaka.com>
2024-10-19 02:07:09 +0600

    add home page tempalte usng fasthtml

---
+++ templates/home_page.py
@@ -0,0 +1,44 @@
+from fasthtml.common import (
+    Div,
+    H1,
+    P,
+    A,
+    Img,
+    Span,
+    Button,
+    Form,
+    Input,
+    Label,
+    Select,
+    Option,
+    Table,
+    Tr,
+    Th,
+    Td,
+)
+
+home_page_template = Div(
+    H1("Welcome to FastHTML", cls="text-3xl font-bold text-teal-600 font-serif"),
+    P("This is a simple HTML template using FastHTML."),
+    A("Learn more", href="https://github.com/anarul/fasthtml"),
+    Img(src="https://via.placeholder.com/500x200", alt="placeholder image"),
+    Span("This is a span tag"),
+    Button("Click me"),
+    Form(
+        Input(type="text", name="name", placeholder="Enter your name"),
+        Label("Email"),
+        Input(type="email", name="email", placeholder="Enter your email"),
+        Label("Message"),
+        Select(
+            name="options",
+            options=[Option("Option 1"), Option("Option 2"), Option("Option 3")],
+        ),
+        Button("Submit"),
+        Table(
+            Tr(Th("Header 1"), Th("Header 2"), Th("Header 3")),
+            Tr(Td("Data 1"), Td("Data 2"), Td("Data 3")),
+            Tr(Td("Data 4"), Td("Data 5"), Td("Data 6")),
+        ),
+    ),
+    cls="container mx-auto",
+)
